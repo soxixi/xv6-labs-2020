302247095327fd3e013b6fd2b737c49020b3b733
soxixi <619145448@qq.com>
2024-03-21 18:08:19 +0800

    [message]

--- kernel/defs.h
+++ kernel/defs.h
@@ -183,8 +183,8 @@ void            vmp(pagetable_t, uint64);
 void            vmprint(pagetable_t);
 pagetable_t     ukvminit();
 void            ukvmmap(pagetable_t, uint64, uint64, uint64, int);
-int             copyin_new(pagetable_t , char , uint64 , uint64 );
-int             copyinstr_new(pagetable_t , char , uint64 , uint64 max);
+int             copyin_new(pagetable_t , char *, uint64 , uint64 );
+int             copyinstr_new(pagetable_t , char * , uint64 , uint64 max);
 int             uvmcopy_not_physical(pagetable_t , pagetable_t , uint64 , uint64 );
 
 
